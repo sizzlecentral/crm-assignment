cde8bd9d17f7906058c0f9b2d872e4a4c0c8909b
Don Cooper <don_cooper@hotmail.com>
2017-05-25 14:23:10 -0400

    CRM class started

--- crm.rb
+++ crm.rb
@@ -1,40 +1,42 @@
 class CRM
 
   def initialize
-
   end
 
   def main_menu
-
+    while true
+      print_main_menu
+      user_selected = gets.to_i
+      call_option(user_selected)
+    end
   end
 
   def print_main_menu
-
+    puts '[1] Add a new contact'
+    puts '[2] Modify and existing contact'
+    puts '[3] Delete a contact'
+    puts '[4] Display all the contacts'
+    puts '[5] Search by attribute'
+    puts '[6] Exit'
+    puts 'Enter a number'
   end
 
   def call_option
-
   end
 
   def add_new_contact
-
   end
 
   def modify_existing_contact
-
   end
 
   def delete_contact
-
   end
 
   def display_all_contacts
-    
   end
 
   def search_by_attribute
-    
   end
-
 
 end
