7a2285595fcff09efde65eaefc3e5514d61c4b6a
Don Cooper <don_cooper@hotmail.com>
2017-05-25 13:00:55 -0400

    Setting up Contact class

--- contact.rb
+++ contact.rb
@@ -1,33 +1,35 @@
 class Contact
 
-  # This method should initialize the contact's attributes
-  def initialize
+  attr_reader :id
+  attr_accessor :first_name, :last_name, :email, :note
 
+  @@contacts = []
+  @@id = 1
+
+  # This method should initialize the contact's attributes
+  def initialize(first_name, last_name, email, note)
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @note = note
   end
 
-  # This method should call the initializer, 
-  # store the newly created contact, and then return it
-  def self.create
+# ----- Class Methods ----- #
 
+  # This method should call the initializer,
+  # store the newly created contact, and then return it
+  def self.create(first_name, last_name, email, note)
+    contact = Contact.new(first_name, last_name, email, note)
+    @@contacts << contact
   end
 
   # This method should return all of the existing contacts
   def self.all
-
   end
 
   # This method should accept an id as an argument
   # and return the contact who has that id
   def self.find
-
-  end
-
-  # This method should allow you to specify 
-  # 1. which of the contact's attributes you want to update
-  # 2. the new value for that attribute
-  # and then make the appropriate change to the contact
-  def update
-
   end
 
   # This method should work similarly to the find method above
@@ -35,24 +37,35 @@ class Contact
   # by specifying both the name of the attribute and the value
   # eg. searching for 'first_name', 'Betty' should return the first contact named Betty
   def self.find_by
-
   end
 
   # This method should delete all of the contacts
   def self.delete_all
+  end
 
+
+# ----- Instance Methods ----- #
+
+  # This method should allow you to specify
+  # 1. which of the contact's attributes you want to update
+  # 2. the new value for that attribute
+  # and then make the appropriate change to the contact
+  def update
   end
 
   def full_name
-
+    "#{first_name} #{last_name}"
   end
 
   # This method should delete the contact
   # HINT: Check the Array class docs for built-in methods that might be useful here
   def delete
-
   end
 
   # Feel free to add other methods here, if you need them.
-  
+
 end
+
+# ----- End of Contact class ----- #
+
+instantiate = Contact.new("Bob", "Jones", "bob@jones.com", "Bob is cool.")
